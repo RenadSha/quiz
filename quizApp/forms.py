85ec79e39121c017bb66fcc664a04867eb6d464a
Renad 10 <116083098+Renad10@users.noreply.github.com>
2025-02-15 20:17:39 +0100

    Design Changes

--- quizApp/forms.py
+++ quizApp/forms.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+
 
 class CreateNewUser(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username','email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Gebruikersnaam'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CreateNewUser, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Wachtwoord'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Bevestig wachtwoord'})
+
+
--- quizProject/settings.py
+++ quizProject/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import os
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIR = [
 	os.path.join(BASE_DIR, 'static')
 ]
